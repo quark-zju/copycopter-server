123e44c916977b1cbd2fef2a0b1d975b98444690
WU Jun <quark@lihdd.net>
2013-03-10 15:06:44 +0800

    serve static files in production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Copycopter::Application.configure do
   config.i18n.fallbacks = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
